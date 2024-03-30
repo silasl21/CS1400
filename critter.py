fc94150029e259189f2b61eb455f7f07a87e02b3
Silas Loosli <looslisilas@gmail.com>
2024-03-29 20:52:55 -0600

    Git rid of them bees

--- critter.py
+++ critter.py
@@ -15,33 +15,42 @@ class Critter:
         self.rect = self.image.get_rect(center=self.position)
 
     def draw(self):
-        self.screen.blit(self.image, None)
+        self.screen.blit(self.image, self.rect)
 
     def move_critter(self):
-        self.position[0] += self.move_speed[0]
-        self.position[1] += self.move_speed[1]
+        self.rect[0] += self.move_speed[0]
+        self.rect[1] += self.move_speed[1]
 
     def check_bounce(self):
         if self.rect.right >= self.screen.get_width() or self.rect.left <= 0:
             self.move_speed[0] *= -1
+
         if self.rect.bottom >= self.screen.get_height() or self.rect.top <= 0:
             self.move_speed[1] *= -1
 
-    def did_get(self, mouse_location):
-        if self.rect.collidepoint(mouse_location):
-            did_get = True
+    def did_get(self, clicking, mouse_location, mouse_type):
+        if self.rect.collidepoint(mouse_location) and clicking:
+            did_get = [True]
+        # if the user got the click right
+            if self.type == "good" and mouse_type == 1 or self.type == "bad" and mouse_type == -1:
+                did_get.append(True)
+
+        # if the user got the click wrong
+            else:
+                did_get.append(False)
         else:
-            did_get = False
+            did_get = [False, None]
         return did_get
 
-    def make_critters_list(self, count, screen, images):
-        good_critter_list = []
-        bad_critter_list = []
-        # count -- the number of critters in the list to return
-        # images -- list of the critter images
-        for i in range(count/2):
-            good_critter_list.append(Critter(screen, "good", "wonk_parrot.png"))
-            bad_critter_list.append(Critter(screen, "bad", "Angry_Bee.png"))
 
-        critters_list = good_critter_list + bad_critter_list
-        return critters_list
+def make_critters_list(count, screen, image):
+    good_critter_list = []
+    bad_critter_list = []
+    # count -- the number of critters in the list to return
+    # images -- list of the critter images
+    for i in range(count // 2):
+        good_critter_list.append(Critter(screen, "good", image[0]))
+        bad_critter_list.append(Critter(screen, "bad", image[1]))
+
+    critters_list = good_critter_list + bad_critter_list
+    return critters_list
--- task1.py
+++ task1.py
@@ -1,7 +1,8 @@
 # Silas Loosli
 # CS1400 - MWF 9:30
+import time
 import pygame
-import critter
+from critter import Critter, make_critters_list
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
@@ -15,39 +16,124 @@ def main():
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
     pygame.display.set_caption(TITLE)
     clock = pygame.time.Clock()
+    count = 10
 
     # Setup media here: #
     # images
     bg_image = pygame.image.load("background.jpg")
+    catch_mouse = pygame.image.load("fishing_rod.png")
+    kill_mouse = pygame.image.load("Arrow_Loaded_Crossbow.png")
 
     # music and sounds
-    bg_music = pygame.mixer.Sound("Minecraft.mp3")
-    catch_sound = pygame.mixer.Sound("catch.mp3")
+    bg_music = pygame.mixer.Sound("Minecraft.mp3")   # assigned to list 0
+    reel_sound = pygame.mixer.Sound("catch.mp3")   # assigned to list in position 1
+    shoot_sound = pygame.mixer.Sound("bow_shoot.mp3")   # assigned to played list in position 2
+    tool_good_sound = pygame.mixer.Sound("Small_xp.mp3")   # assigned to played list in position 3
+    level_complete_sound = pygame.mixer.Sound("Large_xp.mp3")   # position 4
+    bad_sound = pygame.mixer.Sound("Minecraft_oof.mp3")   # position 5
 
-    # setup the game data #
+    # set up the game data #
+    critters_list = make_critters_list(count, screen, ["wonk_parrot.png", "Angry_Bee.png"])
 
     # turn off the mouse
     pygame.mouse.set_visible(False)
 
     # Start the main loop #
-    game_over = False
+    round_over = False
     running = True
-    catch = True
+    win = False
+    mouse_type = 1
+    bg_music.play(-1)
+    init_start_time = time.time()
     while running:
+        clicked = False
         # collect User Input/Events
+        # this is so that the sounds only play once hopefully
+        sounds = [False, False, False, False, False, False]
         mouse_pos = pygame.mouse.get_pos()
+        clicked = pygame.mouse.get_pressed(3)[0]
         # if the user exits the program, break out of the loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
         # Update the game state #
-        # background!
-        screen.blit(bg_image, [0, 0])
-        # Update with blits
 
+        # add in sounds for each item:
+        if clicked:
+            if mouse_type == 1:
+                pygame.mixer.Sound.play(reel_sound)
+            else:
+                pygame.mixer.Sound.play(shoot_sound)
+
+        # move the critters
+        screen.blit(bg_image, [0, 0])
+
+        # handle the critters in critters_list
+        for critter in critters_list:
+            Critter.check_bounce(critter)
+
+            # checking to see if the mouse got clicked
+            if not round_over:
+                if Critter.did_get(critter, clicked, mouse_pos, mouse_type) == [True, False]:
+                    pygame.mixer.Sound.play(bad_sound)
+                    round_over = True
+                    win = False
+
+                elif Critter.did_get(critter, clicked, mouse_pos, mouse_type) == [True, True]:
+                    critters_list.remove(critter)
+                    pygame.mixer.Sound.play(tool_good_sound)
+                    mouse_type *= -1
+
+                if len(critters_list) == 0:
+                    round_over = True
+                    win = True
+
+            Critter.move_critter(critter)
+            Critter.draw(critter)
+
+        # background!
+        # Update with blit
+        # change the pointer-type
+        if mouse_type == 1:
+            mouse_image = catch_mouse
+            draw_pos = [mouse_pos[0] - catch_mouse.get_width() // 1.5, mouse_pos[1] - catch_mouse.get_height() // 1.2]
+        else:
+            mouse_image = kill_mouse
+            draw_pos = mouse_pos
+
+        if round_over:
+            bg_music.stop()
+            # Print the corresponding message to notify the user of the situation
+            if not win:
+                if mouse_type == 1:
+                    sad_message = ["caught", "bee"]
+                else:
+                    sad_message = ["killed", "bird"]
+                final_message = pygame.font.SysFont("timesnewroman", 70).render(
+                    f"You {sad_message[0]} a {sad_message[1]} :(", True, "red")
+            else:
+                pygame.mixer.Sound.play(level_complete_sound, 1, 2)
+                final_message = pygame.font.SysFont("timesnewroman", 70).render(f"You caught the birds and killed the bees!", True, "green")
+
+            # Set a game reset
+            if pygame.key.get_pressed()[pygame.K_SPACE]:
+                round_over = False
+                if win:
+                    count += 10
+                else:
+                    count = 10
+                critters_list = make_critters_list(count, screen, ["wonk_parrot.png", "Angry_Bee.png"])
+                bg_music.play()
+
+            # print the final message to the screen
+            screen.blit(final_message, [SCREEN_WIDTH // 2 - final_message.get_width() // 2, SCREEN_HEIGHT // 2 - final_message.get_height() // 2])
+
+        screen.blit(mouse_image, draw_pos)
         # update the display
+
         pygame.display.flip()
         clock.tick(CLOCK_DELAY)
+
 
 main()
